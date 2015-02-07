bf937a365b3555ce013a69bf8f44c8dd0e1bad11
Mr. Eigenbart <eigenbart@gmail.com>
2015-02-07 09:39:28 +0100

    more refactor

--- lib/rack/jwt/auth.rb
+++ lib/rack/jwt/auth.rb
@@ -10,37 +10,49 @@ module Rack
       end
 
       def call(env)
-        if @exclude.include? env["PATH_INFO"]
+        if @exclude.include? env['PATH_INFO']
           @app.call(env)
-        elsif env["HTTP_AUTHORIZATION"]
-          begin
-            if env["HTTP_AUTHORIZATION"].split(" ").first != 'Bearer'
-              return_error("Invalid Authorization header format")
-            else
-              token = env["HTTP_AUTHORIZATION"].split(" ")[-1]
-              decoded_token = Token.decode(token, @secret)
-              env["jwt.header"]  = decoded_token.last
-              env["jwt.payload"] = decoded_token.first
-              @app.call(env)
-            end
-          rescue
-            return_error("Invalid JWT token")
-          end
         else
-          return_error("Missing Authorization header")
+          check_jwt_token(env)
         end
       end
 
       private
 
-      def return_error(message)
-        body = { error: message }.to_json
-        headers = {
-                    'Content-Type' => 'application/json',
-                    'Content-Length' => message.bytesize.to_s
-                  }
+      def check_jwt_token(env)
+        if valid_header?(env)
+          begin
+            token = env['HTTP_AUTHORIZATION'].split(' ')[-1]
+            decoded_token = Token.decode(token, @secret)
+            env['jwt.header']  = decoded_token.last
+            env['jwt.payload'] = decoded_token.first
+            @app.call(env)
+          rescue
+            return_error('Invalid JWT token')
+          end
+        else
+          return_jwt_header_error(env)
+        end
+      end
 
-        return [401, headers, [body]]
+      def return_jwt_header_error(env)
+        if env['HTTP_AUTHORIZATION'].nil?
+          return_error('Missing Authorization header')
+        elsif env['HTTP_AUTHORIZATION'].split(' ').first != 'Bearer'
+          return_error('Invalid Authorization header format')
+        end
+      end
+
+      def valid_header?(env)
+        env['HTTP_AUTHORIZATION'] =~ /\ABearer \S*\.\S*\.\S*\z/
+      end
+
+      def return_error(message)
+        body    = { error: message }.to_json
+        headers = { 'Content-Type' => 'application/json',
+                    'Content-Length' => body.bytesize.to_s }
+
+        [401, headers, [body]]
       end
     end
   end
