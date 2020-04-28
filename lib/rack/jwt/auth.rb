57812347d094425968c342fe386acb482c353237
Tadej Murovec <tadej.murovec@gmail.com>
2020-04-28 10:42:31 +0200

    Exclude root path only
    This commit allows a dev to only exclude the root path ('/') from
    the JWT token auth check but keep the auth check on other paths.

--- lib/rack/jwt/auth.rb
+++ lib/rack/jwt/auth.rb
@@ -167,7 +167,10 @@ module Rack
       end
 
       def path_matches_excluded_path?(env)
-        @exclude.any? { |ex| env['PATH_INFO'].start_with?(ex) }
+        @exclude.any? { |ex|
+          path = env['PATH_INFO']
+          ex == '/' ? path == ex : path.start_with?(ex)
+        }
       end
 
       def valid_auth_header?(env)
